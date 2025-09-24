bd22a3d14532d58e68353147fb2071f96c1073d9
ESET0098 <kunal.jindal@esyasoft.com>
2025-09-24 17:39:44 +0530

    Add files via upload

---
+++ .Net/class task/Assign/program.cs
@@ -0,0 +1,13 @@
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            hp Hp = new hp();
+            Hp.print();
+            epson Epson = new epson();
+            Epson.print();
+        }
+    }
+}
